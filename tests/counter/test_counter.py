0a30c02d94c771afd06b03664eaf95acc760437a
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-10-21 14:28:06 -0300

    Enviando requisito 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    result = count_ocurrences('src/jobs.csv', 'PYTHON')
+    result_lower = count_ocurrences('src/jobs.csv', 'python')
+    assert result == 1639
+    assert result_lower == 1639
